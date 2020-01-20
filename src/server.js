5f41e9c4380643660a4f5b81ce9d19312f26a4aa
Tumble <tumblegamer@gmail.com>
2020-01-20 12:34:28 +0000

    added options

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-function WebServer(app, port = 3000) {
+function WebServer(app,options = {}, port = 3000) {
     console.log("Starting Web Server created by Tumble1999")
     var port = process.env.PORT || port;
     //var server = http.Server(app);
-    var server = http.createServer(app);
+    var server = http.createServer(options,app);
     var onListen = function () {
         console.log("Server listening on port " + port);
     }
